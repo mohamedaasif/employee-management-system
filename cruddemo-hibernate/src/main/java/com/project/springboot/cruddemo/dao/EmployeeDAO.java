dd7dd04f8d76a6b5368510579d27969bf6f4f470
Aasif-M <Aasif-M@users.noreply.github.com>
2020-08-10 14:50:02 +0530

    First commit

---
+++ cruddemo-hibernate/src/main/java/com/project/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,13 @@
+package com.project.springboot.cruddemo.dao;
+
+import java.util.List;
+
+import com.project.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeDAO {
+
+	public List<Employee> findAll();
+	public Employee findById(int theId);
+	public void save(Employee theEmployee);
+	public void deleteById(int theId);
+}
---
+++ cruddemo-hibernate/src/main/java/com/project/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.project.springboot.cruddemo.service;
+
+import java.util.List;
+
+import com.project.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+	public List<Employee> findAll();
+	public Employee findById(int theId);
+	public void save(Employee theEmployee);
+	public void deleteById(int theId);
+}
