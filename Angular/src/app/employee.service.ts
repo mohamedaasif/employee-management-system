a132a4f5c9f0a269a4dc55f9e61dd6a70f6f5a0c
Aasif-M <52825641+Aasif-M@users.noreply.github.com>
2020-08-11 16:37:04 +0530

    Update employee.service.ts

--- Angular/src/app/employee.service.ts
+++ Angular/src/app/employee.service.ts
@@ -23,10 +23,6 @@ export class EmployeeService {
     return this.http.put(`${this.baseUrl}`, employee);
   }
   
-  // updateEmployee(id: number, value: any): Observable<Object> {
-  //   return this.http.put(`${this.baseUrl}/${id}`, value);
-  // }
-
   deleteEmployee(id: number): Observable<any> {
     return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
   }
